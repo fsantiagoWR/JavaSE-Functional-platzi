5283d790ec46a3dd6e628e84ca0703d3604900d9
fSantiagoWR <felipe.roldan@webratio.com>
2020-04-13 22:41:21 -0500

    SAM y FunctionalInterface

---
+++ modules/src/main/java/com/platzi/functional/_05_sam/AgeUtils.java
@@ -0,0 +1,32 @@
+package com.platzi.functional._05_sam;
+
+import javax.xml.transform.sax.SAXSource;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+public class AgeUtils {
+
+    public static void main(String[] args) {
+
+        Function<Integer, String> addZeros = x -> x<10 ? "0"+x : String.valueOf(x);
+
+        TriFunction<Integer, Integer, Integer, LocalDate> parseDate =
+                (day, month, year) -> LocalDate.parse(year + "-" +
+                        addZeros.apply(month) + "-" + addZeros.apply(day));
+
+        TriFunction<Integer, Integer, Integer, Integer> calculateAge =
+                (day, month, year) -> Period.between(
+                        parseDate.apply(day,month,year),LocalDate.now()
+                        ).getYears();
+
+        System.out.println(calculateAge.apply(14,02,1994));
+    }
+
+    @FunctionalInterface
+    interface TriFunction<T, U, V, R>{
+        R apply(T t,U u,V v); //Funcion que devuelve un R y recibe tres parametros (t,u,v del tipo T,U,V respectivamente)
+    }
+
+
+}
