0480723096d2b119aa763004448288fc4d1c50c3
fSantiagoWR <felipe.roldan@webratio.com>
2020-04-15 16:05:35 -0500

    Stream Listeners

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/Streams.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/Streams.java
@@ -32,5 +32,16 @@ public class Streams {
         Stream<String> justJavaCourses = emphasisCourses.filter(course -> course.contains("Java"));
         justJavaCourses.forEach(System.out::println);
 
+        Stream<String> coursesStream2 = courseList.stream();
+
+        addOperator(
+                coursesStream2.map(course -> course + "!!")
+                .filter(course -> course.contains("Java"))
+        ).forEach(System.out::println);
+
+    }
+
+    static <T> Stream <T> addOperator(Stream<T> stream){
+        return stream.peek(data -> System.out.println("Dato: " + data));
     }
 }
